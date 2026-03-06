29aebbbf5b1571cc367b4bb13197b215eab9e0b3
nishat822 <shahariya.jaman00@gmail.com>
2026-03-07 02:41:12 +0600

    added login form and deshboard file

---
+++ script/login.js
@@ -0,0 +1,21 @@
+function login(){
+
+const username = document.getElementById("username").value
+const password = document.getElementById("password").value
+
+const defaultUser = "admin"
+const defaultPass = "admin123"
+
+if(username === defaultUser && password === defaultPass){
+
+alert("Login Successful 🚀")
+
+window.location.href = "dashboard.html"
+
+}else{
+
+alert("Invalid Username or Password ❌")
+
+}
+
+}
